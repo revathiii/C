ff3a10e6c9db786fd16ad2f2690b16bfd556f968
REVATHI R <78149796+revathiii@users.noreply.github.com>
2023-03-12 11:29:51 +0530

    Update 2power_or_not.c

--- 2power_or_not.c
+++ 2power_or_not.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
-int data;
+int n;
 void main(){
-    printf("enter data:");
-    scanf("%d",&data);
+    printf("enter n:");
+    scanf("%d",&n);
     {
-        if((data&(data-1))==0)
+        if(n&&(n&(n-1))==0)
             printf("2 power");
         else
             printf("Not 2 power");
     }
 
 }
+
