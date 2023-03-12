d4bfd4b70147280cbebaffec1e2aaeae9799a292
REVATHI R <78149796+revathiii@users.noreply.github.com>
2023-03-12 11:12:47 +0530

    Update odd_or_even_without_arithmetic.c

--- odd_or_even_without_arithmetic.c
+++ odd_or_even_without_arithmetic.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
+int data;
 int main(){
-    if((6&1)==0)
+    printf("Enter data:");
+    scanf("%d",&data);
+    {
+
+    if((data&1)==0)
         printf("EVEN");
     else
         printf("ODD");
 
+    }
+
 }
 
-//O/P: EVEN
+//ENTER DATA:6
+//EVEN
+
+//ENTER DATA:9
+//ODD
